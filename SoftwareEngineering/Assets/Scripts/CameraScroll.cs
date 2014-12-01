3e7f87f47c5c74b4002201f5394fc0e4fbae51ac
Travis Crumley <jashiat@gmail.com>
2014-12-01 15:44:49 -0800

    Added camera scrolling and zooming
    As it says

---
+++ SoftwareEngineering/Assets/Scripts/CameraScroll.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScroll : MonoBehaviour {
+
+    float speed = 1000f; //The speed of scrolling
+    Camera camera; //The camera we're manipulating
+
+	// Use this for initialization
+	void Start () {
+        camera = GetComponent<Camera>(); //Grabs the camera from the object this is attached to
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(1)) //If right click is held down
+        {
+            //We're going to adjust the transform of this game object in X and Y directions with respect to the mouse X and Y input inverted
+            //This makes it feel like we're pulling the map around
+            transform.position += new Vector3(-Input.GetAxisRaw("Mouse X") * Time.deltaTime * speed, -Input.GetAxisRaw("Mouse Y") * Time.deltaTime * speed, 0f);
+        }
+        camera.fieldOfView -= 10*Input.GetAxis("Mouse ScrollWheel"); //Adjust the camera's field of view when we scroll, we'll want to limit this.
+    }
+}
