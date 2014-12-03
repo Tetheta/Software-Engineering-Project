def49df11bc579ae408b039b656c469c9833185b
Travis Crumley <jashiat@gmail.com>
2014-12-03 09:51:59 -0800

    Stephen added Networking Code
    Trying to make this commit properly

--- SoftwareEngineering/Assets/Scripts/Networking.cs
+++ SoftwareEngineering/Assets/Scripts/Networking.cs
@@ -1,85 +1,120 @@
 ﻿using UnityEngine;
 using System.Collections;
-//DO NOT RUN THIS AT ALL COSTS. For some reason their is a loop that causes you to refresh the host and check teh connection 4 million times a second
-//I don't know why, but I can't test it because it will no longer let me connect.
 
-public class Networking : MonoBehaviour {
-	string gameName = "CPTR345-Networking-Prototype";
-	bool  refreshing = false;
-	float btnX;
-	float btnY;
-	float btnWidth;
-	float btnHeight;
+public class Networking : MonoBehaviour
+{
+    HostData[] hostData;
+    string gameName = "CPTR345-Networking-Prototype";
+    bool refreshing = false;
+    float btnX;
+    float btnY;
+    float btnWidth;
+    float btnHeight;
 
-	// Use this for initialization
-	void Start () {
-		btnX = Screen.width * 0.05f;
-		btnY = Screen.width * 0.05f;
-		btnWidth = Screen.width * 0.2f;
-		btnHeight = Screen.width *0.05f;
-	}
+    // Use this for initialization
+    void Start()
+    {
+        btnX = Screen.width * 0.05f;
+        btnY = Screen.width * 0.05f;
+        btnWidth = Screen.width * 0.2f;
+        btnHeight = Screen.width * 0.05f;
+    }
 
-	/* This function starts up a server and registers that server to a host. It also makes sure that the connections that are coming in 
-	 * are not public addresses and asserts that they are false.
-	 * The function also registers the game to the initialized server, so people will be able to see that name when trying to make a connection.
-	 */
-	void startServer() {
-		Network.InitializeServer (32, 25004, !Network.HavePublicAddress());
-		MasterServer.RegisterHost (gameName, "Prototype Networking", "This is a test, did it work?");
-	}
+    /* This function starts up a server and registers that server to a host. It also makes sure that the connections that are coming in 
+     * are not public addresses and asserts that they are false.
+     * The function also registers the game to the initialized server, so people will be able to see that name when trying to make a connection.
+     */
+    void startServer()
+    {
+        Network.InitializeServer(32, 25004, !Network.HavePublicAddress());
+        MasterServer.RegisterHost(gameName, "Prototype Networking", "This is a test, did it work?");
+    }
 
-	void refreshHostList (){
-		MasterServer.RequestHostList (gameName);
-		refreshing = true;
-		//Debug.Log(MasterServer.PollHostList().Length);
-	}
+    /* This refreshes the connection to the host to check and see if there are currently any games available.
+     */
+    void refreshHostList()
+    {
+        MasterServer.RequestHostList(gameName);
+        refreshing = true;
+    }
 
-	//    														Debugging Messages
+
+    //    														Debugging Messages
 
 
-	/* This function lets the user know that the instantiation of a server was successful. 
-	 * It piggybacks off of the "Network.InitializeServer" call.
-	 */ 
-	void OnServerInitialized (){
-		Debug.Log ("Server Initialization Successful");
-	}
+    /* This function lets the user know that the instantiation of a server was successful. 
+     * It piggybacks off of the "Network.InitializeServer" call.
+     */
+    void OnServerInitialized()
+    {
+        Debug.Log("Server Initialization Successful");
+    }
 
-	/* This function checks to see if the server was registered to the Unity Master Server successfully or not.
-	 * 
-	 * 
-	 */
-	void OnMasterServerEvent(MasterServerEvent mse){
-		if (mse == MasterServerEvent.RegistrationSucceeded) {
-			Debug.Log("Server Registration Successful");
-		}
+    /* This function checks to see if the server was registered to the Unity Master Server successfully or not.
+     */
+    void OnMasterServerEvent(MasterServerEvent mse)
+    {
+        if (mse == MasterServerEvent.RegistrationSucceeded)
+        {
+            Debug.Log("Server Registration Successful");
+        }
 
-	}
+    }
 
-	/* This function creates buttons that a user can click to perform specific functionalities. One button will allow the user to create a match that other
-	 * users should be able to join. The other button will allow the user to refresh the list of servers that people will be able to view.
-	 * Debug messages are displayed in order to show that the button was selected.
-	 */
-	void OnGUI () {
-		if (GUI.Button (new Rect (btnX, btnY, btnWidth, btnHeight), "Start Server")) {
-						Debug.Log ("Starting Server...");
-						startServer();
-				}
+    /* This function creates buttons that a user can click to perform specific functionalities. One button will allow the user to create a match that other
+     * users should be able to join. The other button will allow the user to refresh the list of servers that people will be able to view.
+     * Debug messages are displayed in order to show that the button was selected.
+     */
+    void OnGUI()
+    {
+        // if (Network.isClient && !Network.isServer)
+        //{
+        if (GUI.Button(new Rect(btnX, btnY, btnWidth, btnHeight), "Start Server"))
+        {
+            Debug.Log("Starting Server...");
+            startServer();
+        }
 
-	//This function creates a button that will eventually allow the user to start a server, or in our case, create a game
-		if (GUI.Button(new Rect(btnX, btnY *1.2f + btnHeight, btnWidth, btnHeight), "Refresh"))
-			Debug.Log ("Refreshing...");
-			refreshHostList ();
-	
-	}
+        //This creates a button that will eventually allow the user to start a server, or in our case, create a game
+        if (GUI.Button(new Rect(btnX, btnY * 1.2f + btnHeight, btnWidth, btnHeight), "Refresh"))
+        {
+            Debug.Log("Refreshing...");
+            refreshHostList();
+        }
 
-	// Update is called once per frame
-	void Update () {
-		if(refreshing){
-			if(MasterServer.PollHostList().Length > 0){
-				refreshing = false;
-				Debug.Log(MasterServer.PollHostList ().Length);
-			}
-		}
-		
-	}
+        /* This creates a button for the game after you have created it and have clicked the refresh hosts button.
+         * Allows a person to "connect" to another game, can't do much with this yet.
+         */
+
+        /* This checks to see if there is a game that currently exists in the HostData array.
+         */
+        if (hostData != null)
+        {
+            // This loops over the array to make sure that the game name is updated if it is changed.
+            for (int i = 0; i < hostData.Length; i++)
+            {
+                //This creates the clickable button for the user created server name.
+                if (GUI.Button(new Rect(btnX * 1.5f + btnWidth, btnY * 1.2f + (btnHeight * i), btnWidth * 3.0f, btnHeight * 0.5f), hostData[i].gameName))
+                {
+                    Network.Connect(hostData[i]);
+                }
+            }
+        }
+        // }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (refreshing)
+        {
+            if (MasterServer.PollHostList().Length > 0)
+            {
+                refreshing = false;
+                Debug.Log(MasterServer.PollHostList().Length);
+                hostData = MasterServer.PollHostList();
+            }
+        }
+
+    }
 }
